a7137117f16a1117f14309e26e9be8f9286e8453
alopedraza94 <49682451+alopedraza94@users.noreply.github.com>
2020-08-05 22:58:17 +0200

    Add files via upload

---
+++ src/org/alopedraza/Solution.java
@@ -0,0 +1,70 @@
+package org.alopedraza;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = {86, 1463, 85, 85, 5798, 2, 85, 2, 86};
+		
+		Solution sol= new Solution();
+		int[] leaders = sol.solution(8, 6000, A);
+		if (leaders.length != 0)
+			Arrays.stream(leaders).boxed().forEach(System.out::println);
+		else
+			System.out.println("Value of array can't be leader");
+	}
+	
+	public int[] solution(int K, int M, int[] A){
+		int[] leaders = {};
+		TreeSet<Integer> leadSet = new TreeSet<Integer>();
+		
+		int maxRankLeader = A.length / 2;
+		
+		if(K < A.length && lessThanMax(M,A)) {
+			int n = 0;
+			while(n + K <= A.length) {
+				//modify the segment in an auxiliary array
+				int[] auxA = A.clone();
+				for(int i = n; i < K + n; i++) {
+					auxA[i]++;
+				}
+				
+				//check how many times each number is repeated
+				for(int i = 0; i < auxA.length; i++) {
+					int nRepeat = 0;
+					for(int j = i; j < auxA.length; j++) {
+						if(auxA[i] == auxA[j]) {
+							nRepeat++;
+						}
+					}
+					if(nRepeat > maxRankLeader) {
+						leadSet.add(auxA[i]);
+						break;
+					}
+				}
+				n++;
+			}
+		}
+		
+		//convert set in array to return
+		if(!leadSet.isEmpty())
+			leaders = leadSet.stream().mapToInt(lead -> lead.intValue()).toArray();
+		
+		return leaders;
+	}
+	
+    //function to know if any array value exceeds the max
+	private boolean lessThanMax(int max, int[] A) {
+		for(int a : A) {
+			if(a > max)
+				return false;
+		}
+		
+		return true;
+	}
+
+}
